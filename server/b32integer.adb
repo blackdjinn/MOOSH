d68fa45f8bd19d19ff530d1f93d341094aaad35d
Ryan Davis <iconoclasmandheresy@gmail.com>
2013-05-04 23:29:47 -0700

    It compiles...

--- server/b32integer.adb
+++ server/b32integer.adb
@@ -9,10 +9,12 @@ package body B32Integer is
 
 
    function Charval(C:Character) return b32U is
+      S:String(1..1);
    begin
+      S(1):=C;
       case C is
          when 'I' | 'i' | 'L' | 'l' => return 1;
-         when '0'..'9' => return Character'Pos(C) - Character'Pos('0');
+         when '0'..'9' => return b32Char'Pos(B32Char'Value(S));
          when 'A'..'H' => return Character'Pos(C) - Character'Pos('A')+ 10;
          when 'a'..'h' => return Character'Pos(C) - Character'Pos('A')+10;
          when others => return 200;
