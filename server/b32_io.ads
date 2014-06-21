6cb7f99ca1895c5a3450741ca70dacaddb649cba
Ryan Davis <iconoclasmandheresy@gmail.com>
2014-06-21 03:08:59 -0700

    Cleanup removal of b32 stuff

--- server/b32_io.ads
+++
@@ -1,3 +0,0 @@
-with B32Integer;
-with Ada.Text_IO;
-package b32_IO is new Ada.Text_IO.Integer_IO (B32Integer.b32U);
--- server/b32integer.ads
+++
@@ -1,6 +0,0 @@
-package B32Integer is
-   subtype b32U is Integer range 0..16#7FFFFFFF#;
-   function Charval(C:Character) return b32U;
-   function Scan (S:String) return b32U;
-   function Format (N:b32U) return String;
-end B32Integer;
