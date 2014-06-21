4eb72352efd33f5e5541ad573fa488125cf7a5b6
Ryan Davis <iconoclasmandheresy@gmail.com>
2014-06-21 03:04:43 -0700

    base32 was lame, stop using it.

--- server/moosh.adb
+++ server/moosh.adb
@@ -1,16 +1,10 @@
 with NetConnect;
 with Logger; use Logger;
-with B32Integer;
-with b32_IO; use b32_IO;
 
 
 procedure moosh is
 begin
    Log("MOOSH running.");
    NetConnect.Testr;
-   Log(B32Integer.Format(135));
-   Put(B32Integer.Charval('3'));
-   Put(B32Integer.Charval('C'));
-   Put(B32Integer.Charval('X'));
    Log("MOOSH closing.");
 end moosh;
